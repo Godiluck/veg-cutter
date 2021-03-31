9d905a09379f07a8f2db7201a05aa4ad3c1bd24d
Godiluck <givan3819@gmail.com>
2021-03-31 21:29:05 +0300

    Initial commit

---
+++ index_files/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+
+    $("a[href^='#']").click(function () {
+        var _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top + "px"});
+        return false;
+    });
+
+     $('.reviews_list4').slick({
+        infinite: true,
+        adaptiveHeight: true,
+        dots: false,
+        arrows: true,
+        fade: false,
+        speed: 300,
+        autoplay: false,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+
+    $('[name="country"]').on('change', function () {
+        var geoKey = $(this).find('option:selected').val();
+        var data = $jsonData.prices[geoKey];
+        var price = data.price;
+        var oldPrice = data.old_price;
+        var currency = data.currency;
+        $("[value = " + geoKey + "]").attr("selected", true).siblings().attr('selected', false);
+
+        $('.price_land_s1').text(price);
+        $('.price_land_s2').text(oldPrice);
+        $('.price_land_curr').text(currency);
+    });
+});
